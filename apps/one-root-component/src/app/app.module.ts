0033d41d3ff4f1d39b7162665cc94707f353df8e
MarioGeier00 <marioegbertgeier@gmail.com>
2022-03-24 08:03:45 +0100

    initial commit

---
+++ apps/one-root-component/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {SharedUiModule} from "@angular-index-html/shared-ui";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], {initialNavigation: 'enabledBlocking'}),
+    SharedUiModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}
